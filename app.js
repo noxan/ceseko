1fc025834593293e8acbc91de7918fb0dd683c0e
noxan <noxan@byteweaver.net>
2013-06-23 17:34:37 +0200

    configure express app in app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var http = require('http');
+var path = require('path');
 
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+
+  app.use(express.favicon());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
 
 app.configure('development', function() {
   app.use(express.errorHandler());
