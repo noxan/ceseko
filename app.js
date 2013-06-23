4870dc3e0722335277f48fb8fbb3c03d5d05a118
noxan <noxan@byteweaver.net>
2013-06-23 17:20:04 +0200

    add error handler for development

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var http = require('http');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
+app.configure('development', function() {
+  app.use(express.errorHandler());
+});
+
 http.createServer(app).listen(app.get('port'), function() {
   console.log("App running on port " + app.get('port'));
 });
