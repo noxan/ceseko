9e35f1902a9f500900b1e369ea356d1c9daa24ed
noxan <noxan@byteweaver.net>
2013-06-23 17:48:50 +0200

    add training log schema

--- db.js
+++ db.js
@@ -1,3 +1,18 @@
 var mongoose = require('mongoose');
+var db = mongoose.connect('localhost/ceseko');
 
-mongoose.connect('localhost/ceseko');
+var TrainingLogSchema = mongoose.Schema({
+  username: String,
+  content: String,
+  trainingDate: {
+    type: Date,
+    required: true
+  },
+  creationDate: {
+    type: Date,
+    default: Date.now,
+    required: true
+  }
+});
+
+var TrainingLog = mongoose.model('TrainingLog', TrainingLogSchema);
