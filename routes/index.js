f3738afe7100a849e578d778f5c804d91136ac0e
noxan <noxan@byteweaver.net>
2013-06-23 18:47:40 +0200

    implement create route for training log

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,19 @@
+var mongoose = require('mongoose');
+var TrainingLog = mongoose.model('TrainingLog');
+
+
 exports.index = function(req, res) {
   res.render('index', {
     title: 'ceseko'
   });
-}
+};
+
+exports.create = function(req, res) {
+  new TrainingLog({
+    username: req.body.username,
+    content: req.body.conent,
+    trainingDate: req.body.trainingDate
+  }).save(function(err, trainingLog, count) {
+    res.redirect('/');
+  });
+};
